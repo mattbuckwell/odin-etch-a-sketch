9ef400312668c3daecbc4823f3942409ef75af1b
Matt Buckwell <mattbuckwell@gmail.com>
2024-11-18 15:48:35 +1300

    Added code to app.js to display the game board and container

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector("#game-container");
+const gameBoard = document.createElement("div");
+
+gameBoard.classList.add("game-board");
+
+container.appendChild(gameBoard);
