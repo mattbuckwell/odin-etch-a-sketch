0ddef8203fc234638752cfb8214e5082b932b0d6
Matt Buckwell <mattbuckwell@gmail.com>
2024-12-02 12:33:00 +1300

    update the game board with x number of divs

--- js/app.js
+++ js/app.js
@@ -3,4 +3,11 @@ const gameBoard = document.createElement("div");
 
 gameBoard.classList.add("game-board");
 
+for(let i = 0; i < 10; i++) {
+    let div = document.createElement("div"+[i]);
+    div.classList.add("game-square");
+    div.textContent = i+1;
+    gameBoard.appendChild(div);
+}
+
 container.appendChild(gameBoard);
