5de9cbeb93029363fe4d8406a471405687b98bae
Matt Buckwell <mattbuckwell@gmail.com>
2024-12-12 12:41:39 +1300

    Updated spelling of changed variable

--- js/app.js
+++ js/app.js
@@ -34,13 +34,13 @@ function createGrid (gridSize) {
     const boxSize = ((500/gridSize)-2);
     for (let i = 0; i < (gridSize*gridSize); i++) {
         let gridBox = document.createElement("div");
-        girdBox.classList.add("grid-box");
-        girdBox.style.width = boxSize.toString() + "px";
-        girdBox.style.height = boxSize.toString() + "px";
+        gridBox.classList.add("grid-box");
+        gridBox.style.width = boxSize.toString() + "px";
+        gridBox.style.height = boxSize.toString() + "px";
         gridBox.addEventListener("mouseover", () => {
             gridBox.style.backgroundColor = "red";
         })
-        gameBoard.appendChild(box);
+        gameBoard.appendChild(gridBox);
     }
 }
 
