ffa79828274d24bd1f8195293bf0876d585533b4
Matt Buckwell <mattbuckwell@gmail.com>
2024-12-11 13:20:13 +1300

    Reset the project to start again

--- js/app.js
+++ js/app.js
@@ -1,13 +1,1 @@
-const container = document.querySelector("#game-container");
-const gameBoard = document.createElement("div");
 
-gameBoard.classList.add("game-board");
-
-for(let i = 0; i < 10; i++) {
-    let div = document.createElement("div"+[i]);
-    div.classList.add("game-square");
-    div.textContent = i+1;
-    gameBoard.appendChild(div);
-}
-
-container.appendChild(gameBoard);
