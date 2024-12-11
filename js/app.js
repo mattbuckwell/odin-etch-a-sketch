29c0a56d1caf2a43741ac0e0f1307cfc3853b7d6
Matt Buckwell <mattbuckwell@gmail.com>
2024-12-11 14:02:43 +1300

    Added function to create the grid based on the size

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ startBtn.textContent = "Create Grid";
 startBtn.classList.add("btn");
 startBtn.addEventListener("click", () => {
     const gridSize = prompt("What size grid would you like? (Pick between 8 - 16)");
-    console.log(gridSize);
+    createGrid(gridSize);
 })
 
 const clearBtn = document.createElement("button");
@@ -23,6 +23,20 @@ const gameBoard = document.createElement("div");
 gameBoard.style.width = "500px";
 gameBoard.style.height = "500px";
 gameBoard.style.border = "1px solid black";
+gameBoard.classList.add("game-board");
+
+
+// ***** Grid Function *****
+function createGrid (gridSize) {
+    const divSize = ((500/gridSize)-2);
+    for (let i = 0; i < (gridSize*gridSize); i++) {
+        let box = document.createElement("div");
+        box.style.width = divSize.toString() + "px";
+        box.style.height = divSize.toString() + "px";
+        box.style.border = "1px solid black";
+        gameBoard.appendChild(box);
+    }
+}
 
 
 // ***** Attaching Elements *****
