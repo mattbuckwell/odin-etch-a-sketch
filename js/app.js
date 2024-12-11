d81b01af3e2bfbf0c758699e14160bf19310b33d
Matt Buckwell <mattbuckwell@gmail.com>
2024-12-11 13:30:52 +1300

    Add the title and buttons to index.html for the project

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+const container = document.querySelector("#container");
 
+const title = document.createElement("h1");
+title.textContent = "Etch-A-Sketch";
+
+const startBtn = document.createElement("button");
+startBtn.textContent = "Create Grid";
+startBtn.classList.add("btn");
+
+const clearBtn = document.createElement("button");
+clearBtn.textContent = "Clear Grid";
+clearBtn.classList.add("btn");
+
+container.appendChild(title);
+container.appendChild(startBtn);
+container.appendChild(clearBtn);
